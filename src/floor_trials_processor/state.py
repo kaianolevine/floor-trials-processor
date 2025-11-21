b022dce9dc1207e875e4a73bed45e2abc1cfb6ad
Kaiano Levine <kaiano.levine@gmail.com.com>
2025-11-21 11:53:00 -0600

    Cleaning up, fixing raw submissions

--- src/floor_trials_processor/state.py
+++ src/floor_trials_processor/state.py
@@ -35,6 +35,11 @@ class SpreadsheetState:
                 "data": [],
                 "cols": 5,
             },
+            "raw_submissions": {
+                "range": config.RAW_SUBMISSIONS_RANGE,
+                "data": [],
+                "cols": 5,
+            },
             "rejected_submissions": {
                 "range": config.REJECTED_SUBMISSIONS_RANGE,
                 "data": [],
